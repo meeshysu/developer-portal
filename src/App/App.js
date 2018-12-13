3dd4a15dd3a47104b4c1437b1f7e37ff2048c77b
Michelle Beshears <meeshysu@gmail.com>
2018-12-12 20:32:25 -0600

    logging into github works

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Auth from '../components/Auth/Auth';
+import connection from '../Helpers/Data/connection';
 import MyNavbar from '../components/MyNavbar/MyNavbar';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    connection();
+  }
+
   render() {
     return (
       <div className="App">
