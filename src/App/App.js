f75c9316fa67dee3b74aaa7a6759325b883d4459
Michelle Beshears <meeshysu@gmail.com>
2018-12-12 19:55:40 -0600

    api keys up

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth from '../components/Auth/Auth';
+import MyNavbar from '../components/MyNavbar/MyNavbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button className='btn btn-danger'>HELP ME</button>
-          <Button
-            tag="a"
-            color="success"
-            size="large"
-            href="http://reactstrap.github.io"
-            target="_blank"
-          >
-            View Reactstrap Docs
-          </Button>
-        </header>
+        <MyNavbar />
+        <Auth />
       </div>
     );
   }
